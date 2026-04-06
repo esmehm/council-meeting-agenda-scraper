43c5e25f62d4cf8fdbadcb7c8ac30010e34e3d48
Esme HM <61033090+esmehm@users.noreply.github.com>
2026-04-06 22:19:32 +1200

    functioning kuringgai scraper

--- aus_council_scrapers/scrapers/nsw/kuringgai.py
+++ aus_council_scrapers/scrapers/nsw/kuringgai.py
@@ -6,6 +6,6 @@ class KuRingGaiScraper(InfoCouncilScraper):
     def __init__(self):
         council = "kuringgai"
         state = "NSW"
-        base_url = "http://eservices.kmc.nsw.gov.au/"
-        infocouncil_url = "https://eservices.kmc.nsw.gov.au/Infocouncil.Web/"
+        base_url = "https://www.krg.nsw.gov.au"
+        infocouncil_url = "https://kuringgai.infocouncil.biz/"
         super().__init__(council, state, base_url, infocouncil_url)
